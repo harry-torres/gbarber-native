1001cb86bbfa4370c608677224ae853faa3dfe72
Henrique Alexandre Torres <torresrick.dev@gmail.com>
2019-12-18 22:28:54 -0400

    Add sign up page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,13 +1,56 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+import Background from '~/components/Background';
 
-import Background from '../../components/Background';
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+import logo from '~/assets/logo.png';
 
-export default function SignUp() {
+export default function SignUp({ navigation }) {
   return (
     <Background>
-      <Text>SignUp 2</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            placeholder="Nome Completo"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu email"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+          <SubmitButton
+            onPress={() => {
+              navigation.navigate('SignUp');
+            }}
+          >
+            Acessar
+          </SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
